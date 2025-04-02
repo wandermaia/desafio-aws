043758d7965c8e9750619281a709fef9056f9a18
Wander Maia <wander.silva@semeartech.com.br>
2025-04-02 11:33:53 -0300

    Ajuste no arquivo de outputs do terraform

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,11 +3,6 @@ output "cluster_endpoint" {
   value       = module.eks_cluster.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks_cluster.cluster_security_group_id
-}
-
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks_cluster.cluster_name
