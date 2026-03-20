b499ae7cf5763ccc81a9334e3755387c5e677d98
Wander Maia <wandermaia@yahoo.com.br>
2026-03-20 13:49:04 -0300

    Disable debug mode in app.run()
    Removed debug mode from app.run() in main block.

--- src/magic-calculator/app.py
+++ src/magic-calculator/app.py
@@ -39,4 +39,5 @@ def voltar():
   return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0')
